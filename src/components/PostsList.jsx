3321325600059ad546f2e89e8d48e6d8006cfdb0
unknown <fredericreact@gmail.com>
2023-01-28 13:43:23 +0800

    Handling Form Submission

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,31 +5,19 @@ import NewPost from './NewPost'
 import Modal from './Modal'
 
 const PostsList = ({isPosting, onStopPosting}) => {
-const [enteredBody,setEnteredBody] = useState('')
-const [enteredAuthor, setEnteredAuthor] = useState('')
-
-const bodyChangeHandler = (event) => {
-    setEnteredBody(event.target.value)
-}
-
-const authorChangeHandler = (event) => {
-    setEnteredAuthor(event.target.value)
-}
 
 return (
 <>
 {isPosting && 
 <Modal onClose={onStopPosting}>
 <NewPost 
-onBodyChange={bodyChangeHandler} 
-onAuthorChange={authorChangeHandler}
+
 onCancel ={onStopPosting}    
 />
 </Modal> 
 }
 
 <ul className={classes.posts}>
-<Post author={enteredAuthor} body={enteredBody}/>
 <Post author="John" body="Vue"/>
 </ul>
 </>

602e430df2481f2f545470bded58caaa739f7758
unknown <fredericreact@gmail.com>
2023-01-28 12:40:11 +0800

    Preparing the App For State Management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
 import classes from './PostsList.module.css'
+import NewPost from './NewPost'
 
 const PostsList = () => {
 return (
+<>
+<NewPost/>
 <ul className={classes.posts}>
 <Post author="Fred" body="react"/>
 <Post author="John" body="Vue"/>
 </ul>
+</>
 )
 }
 

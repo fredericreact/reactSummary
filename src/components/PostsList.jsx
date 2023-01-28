d6508491c58c5cc8e3d416a9616d93ea4bc9c547
unknown <fredericreact@gmail.com>
2023-01-28 13:57:15 +0800

    Outputting List Data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,7 +5,7 @@ import NewPost from './NewPost'
 import Modal from './Modal'
 
 const PostsList = ({isPosting, onStopPosting}) => {
-const [posts,setPosts] = useState({})
+const [posts,setPosts] = useState([])
 
 const addPostHandler = (postData) => {
     setPosts((existingPosts)=>[postData, ...existingPosts])
@@ -22,10 +22,17 @@ onAddPost={addPostHandler}
 />
 </Modal> 
 }
-
-<ul className={classes.posts}>
-<Post author="John" body="Vue"/>
+{posts.length>0 && (
+    <ul className={classes.posts}>
+{posts.map((post)=> <Post key={post.body} author={post.author} body={post.body}/>)}
 </ul>
+)}
+{posts.length === 0 && (
+    <div style={{textAlign: 'center', color: 'white'}}>
+        <h2>There are no posts yet</h2>
+        <p>Start adding some</p>
+    </div>
+    )}
 </>
 )
 }

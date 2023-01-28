ad98e658f4f121618fb67577c3ed0ea72315fcdf
unknown <fredericreact@gmail.com>
2023-01-28 13:27:41 +0800

    Adding a Shared Header & More State Management

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import PostsList from "./components/PostsList";
-
+import MainHeader from './components/MainHeader'
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  const showModalHandler = () => {
+    setModalIsVisible(true)
+  }
+
+  const hideModalHandler =() => {
+    setModalIsVisible(false)
+}
+
   return (
+    <>
+    <MainHeader onCreatePost={showModalHandler}/>
   <main>
-   <PostsList/>
+   <PostsList isPosting={modalIsVisible} onStopPosting={hideModalHandler}/>
   </main>
+  </>
   )
 }
 
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,14 +4,9 @@ import classes from './PostsList.module.css'
 import NewPost from './NewPost'
 import Modal from './Modal'
 
-const PostsList = () => {
-const [modalIsVisible, setModalIsVisible] = useState(true);
+const PostsList = ({isPosting, onStopPosting}) => {
 const [enteredBody,setEnteredBody] = useState('')
 const [enteredAuthor, setEnteredAuthor] = useState('')
-
-const hideModalHandler =() => {
-    setModalIsVisible(false)
-}
 
 const bodyChangeHandler = (event) => {
     setEnteredBody(event.target.value)
@@ -23,8 +18,8 @@ const authorChangeHandler = (event) => {
 
 return (
 <>
-{modalIsVisible && 
-<Modal onClose={hideModalHandler}>
+{isPosting && 
+<Modal onClose={onStopPosting}>
 <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
 </Modal> 
 }
